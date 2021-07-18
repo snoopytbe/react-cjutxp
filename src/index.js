52a765ea0cd28d2ae4f2bfa083f4f9f85fd498ad
snoopytbe <matthieu.andre.77@gmail.com>
2021-07-18 17:38:07 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import Auth0ProviderWithHistory from "./auth/Auth0ProviderWithHistory";
 
-ReactDOM.render(
-  <Router>
-    <Auth0ProviderWithHistory>
-      <App />
-    </Auth0ProviderWithHistory>
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render(document.getElementById("root"));
